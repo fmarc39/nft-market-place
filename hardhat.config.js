f9b0ae83e62ecf3b120e2de369d11af6b94d33fa
Marc François <fmarc@MacBook-Pro-de-Marc.local>
2021-08-02 19:50:21 +0200

    merge

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,16 +2,7 @@ require("@nomiclabs/hardhat-waffle");
 const fs = require("fs");
 
 const privateKey = fs.readFileSync(".secret").toString();
-const projectId = "0bde4451fec140b0b6908eb541d38ea9";
-
-// This is a sample Hardhat task. To learn how to create your own go to
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const projectId = fs.readFileSync(".infuraid").toString().trim();
 
 module.exports = {
   networks: {
@@ -27,5 +18,13 @@ module.exports = {
       accounts: [privateKey],
     },
   },
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
